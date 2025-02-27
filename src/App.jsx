f62f779f391dd72502ad74e268778100b942b1cb
Harishkumaresan <harishkumaresan14@gmail.com>
2025-02-27 13:23:24 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
-
-import viteLogo from '/vite.svg'
+import {Route, Routes} from 'react-router-dom'
+import {Box} from '@mui/material'
 import './App.css'
+import Home from './Pages/Home'
+import ExerciseDetail from './Pages/ExerciseDetail'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      hello
-    </>
+  <Box width={'400px'} sx={{width:{ xl: '1488px'}}} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/exercise/:id' element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+ </Box>
   )
 }
 
