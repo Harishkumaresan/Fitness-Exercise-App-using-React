b9f1b1a600617dac2fb83944bc41057261838472
Harishkumaresan <harishkumaresan14@gmail.com>
2025-02-28 12:34:53 +0530

    Search field work todays with the api

---
+++ src/utils/fetchData.js
@@ -0,0 +1,15 @@
+export const exercoseOptions ={
+        method: 'GET',
+        headers: {
+          'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY,
+          'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+        }
+};
+
+
+export const fetchData1 = async (url , options) =>{
+    const response = await fetch(url ,options);
+    const data = await response.json();
+
+    return data ;
+}
